4b8aef0bf357c13605f3a6bc08fa603a3177f32c
hopeandlearn <anabilsalah@gmail.com>
2022-08-21 04:59:07 +0200

    refactored controller to become restful

--- src/main/java/com/example/bas/billing_account_service/controllers/SubscriberController.java
+++ src/main/java/com/example/bas/billing_account_service/controllers/SubscriberController.java
@@ -3,10 +3,16 @@ package com.example.bas.billing_account_service.controllers;
 import com.example.bas.billing_account_service.models.Subscriber;
 import com.example.bas.billing_account_service.services.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/vois/internship-program/v1")
@@ -17,19 +23,33 @@ public class SubscriberController {
 
     @GetMapping("/{ban}/subscribers")
     @ResponseStatus(HttpStatus.OK)
-    List<Subscriber> getSubscribers(@PathVariable(value = "ban") String ban) {
-        return subscriberService.getSubscribersByBan(ban);
+    CollectionModel<EntityModel<Subscriber>> getSubscribers(@PathVariable(value = "ban") String ban) {
+        List<EntityModel<Subscriber>> subscribers = subscriberService.getSubscribersByBan(ban).stream()
+                .map(subscriber -> EntityModel.of(subscriber,
+                        WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).getSubscriber(
+                                ban, subscriber.getId())).withSelfRel(),
+                        WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).getSubscribers(ban)).withRel("subscribers")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(subscribers, WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).getSubscribers(ban))
+                .withSelfRel());
     }
 
     @GetMapping("/{ban}.{subscriber}")
-    Subscriber getSubscriber(@PathVariable(value = "ban") String ban, @PathVariable(value = "subscriber") String subscriberId) {
+    EntityModel<Subscriber> getSubscriber(@PathVariable(value = "ban") String ban, @PathVariable(value = "subscriber") String subscriberId) {
 
-        return subscriberService.getSubscriber(ban, subscriberId);
+        return EntityModel.of(subscriberService.getSubscriber(ban, subscriberId), //
+                WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).getSubscriber(ban, subscriberId)).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).getSubscribers(ban)).withRel("subscribers"));
     }
 
     @PostMapping("/subscriber")
-    Subscriber createSubscriber(@RequestBody Subscriber subscriber) {
-        return subscriberService.createSubscriber(subscriber);
+    EntityModel<Subscriber> createSubscriber(@RequestBody Subscriber subscriber) {
+        subscriber = subscriberService.createSubscriber(subscriber);
+        return EntityModel.of(subscriber, //
+                WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).
+                        getSubscriber(subscriber.getBan(), subscriber.getId())).withSelfRel(),
+                WebMvcLinkBuilder.linkTo(methodOn(SubscriberController.class).getSubscribers(subscriber.getBan())).withRel("subscribers"));
     }
 
     @ResponseBody
